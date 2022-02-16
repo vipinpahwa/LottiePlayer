4bfc231c108af360e721dfd45147c3f095b4d918
vipin.pahwa <vipin.pahwa@go-jek.com>
2022-02-16 18:38:48 +0530

    Add text field in view controller

--- LottiePlayer/ViewController.swift
+++ LottiePlayer/ViewController.swift
@@ -14,14 +14,39 @@ class ViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    private let urlField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.placeholder = "Enter url here...."
+        textField.borderStyle = .roundedRect
+        textField.textColor = .black
+        textField.returnKeyType = .continue
+        textField.autocorrectionType = .no
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        urlField.delegate = self
         self.view.addSubview(titleLabel)
         NSLayoutConstraint.activate([
             self.titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             self.titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
             self.titleLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
+        
+        self.view.addSubview(urlField)
+        NSLayoutConstraint.activate([
+            self.urlField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.urlField.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),
+            self.urlField.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
     }
 }
