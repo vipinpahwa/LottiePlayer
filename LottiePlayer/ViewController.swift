c0c2e8113ab79d9e3fd62d5b5c5c2d271c2cc546
vipin.pahwa <vipin.pahwa@go-jek.com>
2022-02-16 17:42:15 +0530

    Initial Commit

---
+++ LottiePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottiePlayer
+//
+//  Created by Vipin Pahwa on 16/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
